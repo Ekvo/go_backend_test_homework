4e83257d0dbda7f501c17637f8a4f56538a7ce98
aleksanderburlin <f.h.elventor@yandex.ru>
2024-11-07 19:31:57 +0300

    Add first commit

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 )
-
+//Функция Add берёт две переменные типа int,
+//производит операцию сложения,
+//возвращает результат типа int
 func Add(a, b int) int {
 	return a + b
 }
